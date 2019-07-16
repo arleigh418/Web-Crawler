27a4612d1dd3af00252fdb4186ac6feb322731cf
張峮瑋(Arleigh Chang) <40189046+arleigh418@users.noreply.github.com>
2019-07-16 15:55:12 +0800

    Update Gihun_bug.py

--- Gihun_bug.py
+++ Gihun_bug.py
@@ -4,62 +4,68 @@ import re
 from urllib.request import urlopen
 from selenium import webdriver
 import time
-
+import pandas as pd
 driver = webdriver.Chrome()
 driver.implicitly_wait(3)
 driver.get('https://news.cnyes.com/news/cat/tw_stock')
 
 
-for i in range(1,10):
-    driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+
+# Get scroll height
+
+for i in range(0,20):
+    driver.execute_script("window.scrollTo(0, 500)")
     time.sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(0.5)
+
+
+
+
 soup = BeautifulSoup(driver.page_source)
 
 gihun = []
 
-for index,ele in enumerate(soup.find("div","_2bF theme-list").find_all('a'),0):
-    gg_url = "https://news.cnyes.com" + ele.get('href')
-    gihun.append(gg_url)
-    # gg_url  
-
-i = 0
-z1 = open("123456789.txt",'w',encoding = 'utf-8')
-for i,gihun_all in enumerate(gihun):  
-    res =  requests.get(gihun_all)
-    soup = BeautifulSoup(res.text,'html.parser')
-    title = soup.find('span',{'class':'_uo1'}).text
-    date = soup.find('time').text
-   
-    
-    if title == None:
-        continue
-    
+for index,ele in enumerate(soup.find_all("a","_1Zdp")):
     try:
-            
-        for item in soup.find_all('div',{'class':'_1Uu'},limit=1):
-            content = item.text
-            if content == None:
-                continue
+        gg_url = "https://news.cnyes.com" + ele.get('href')
+        gihun.append(gg_url)
+    except TypeError:
+        print('GET NONE')
+        pass
+
+title = []
+date = []
+content = []
+url = []  
+non = 0
+for i in gihun:
+    url.append(i)
+    res =  requests.get(i)
+    soup = BeautifulSoup(res.text,'html.parser')
+    try:
+        print('===================================')
+        title.append(soup.find('h1').text)
+        print(soup.find('h1').text)
+        date.append(soup.find('time').text)
+        print(soup.find('time').text)
+        content.append(soup.find('div',{'class':'_1UuP'}).text)
         
-            # z1.write('\n')
-            # z1.write('鉅亨網')
-            # z1.write('\n')
-            z1.write(title)
-            z1.write('\n')
-            # z1.write("日期:"+date)
-            # z1.write('\n')
-            z1.write(content)
-            z1.write('\n')
-            # z1.write("網址:"+gihun_all)
-            # z1.write('\n')
-            # z1.write('===================================================')
-            z1.write('\n')
-            # # z1.write('=============原始碼===============')
-            # # z1.write(single_news_str)
-            # z1.write('=======================================================')
-    except:
-        continue
+       
+    except AttributeError:
+        non+=1
+        pass
+   
+print(len(title))
+print(len(date))
+print(len(content))
+pd_store= pd.DataFrame(columns = ['標題','時間','內容','網址'])
 
+pd_store['標題'] = title
+pd_store['時間'] = date
+pd_store['內容'] = content
+pd_store['網址'] = url
 
-
-    
+pd_store.to_excel('TEST.xlsx')   
+print('總爬取網址:',len(gihun))  
+print('無效網址:',non)   
